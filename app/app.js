7e88055042e765ba453558d0e54a62302713c90b
Rodgers Kishagha <mkishagha@gmail.com>
2018-10-16 22:55:54 +0300

    setup test routes for the database models

--- app/app.js
+++ app/app.js
@@ -1,3 +1,44 @@
-const express = require ('express')
-const app = express ();
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('../database/models')
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.post('/api/users', (req, res) => db.User.create({
+  firstName: req.body.first_name,
+  lastName: req.body.last_name,
+  email: req.body.email,
+})
+  .then(user => res.status(201).send(user))
+  .catch(err => res.status(400).send(err)))
+
+app.get('/api/users/:id', (req, res) => db.User.find({
+  where: { id: req.params.id },
+  include: [
+    {
+      model: db.Book,
+      attributes: ['title'],
+      as: 'AuthoredBooks',
+      through: { attributes: [] },
+    },
+  ],
+})
+  .then(user => res.status(200).send(user))
+  .catch(err => res.status(400).send(err)))
+
+app.get('/api/book/:id', (req, res) => db.Book.findById(req.params.id)
+  .then(book => res.status(200).send(book))
+  .catch(err => res.status(400).send(err)))
+
+app.post('/api/books', (req, res) => db.Book.create({
+  title: req.body.title,
+})
+  .then((book) => {
+    book.setAuthors([1])
+    res.status(201).send(book)
+  })
+  .catch(err => res.status(400).send(err)))
+
+
 module.exports = app;
