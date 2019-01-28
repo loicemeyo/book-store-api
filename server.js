6e5eb9edfcee5d8af91152ee4350a6278bee06fd
Rodgers Kishagha <mkishagha@gmail.com>
2019-01-28 09:27:14 +0300

    user logger instead of console.log

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const logger = require('fancy-log')
 const app = require('./app/app')
 const config = require('./config/config')
 
 const { port } = config
 app.listen(port, () => {
-  console.log('Magic happening on port', port)
+  logger.info('Magic happening on port', port)
 })
