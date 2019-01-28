a0697342f30afd5248bab33a4b6ec60efa06dcf5
Rodgers Kishagha <mkishagha@gmail.com>
2019-01-28 16:40:45 +0300

    feat(config): add config validations

--- config/config.js
+++ config/config.js
@@ -1,14 +1,35 @@
 require('dotenv').config();
+const joi = require('joi')
+
+const envVarsSchema = joi.object({
+  NODE_ENV: joi.string()
+    .allow(['development', 'production', 'test', 'staging'])
+    .required(),
+  PORT: joi.number().default(8080),
+  DATABASE: joi.string().required(),
+  DATABASE_DIALECT: joi.string().default('postgres'),
+  DATABASE_PASSWORD: joi.string().default(null),
+  DATABASE_USER: joi.string().required(),
+  HOST: joi.string().required(),
+})
+  .unknown()
+  .required()
+
+const { error, value: envVars } = joi.validate(process.env, envVarsSchema)
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
+}
 
 
 const config = {
-  port: process.env.PORT || 8000,
-  database: process.env.DATABASE,
-  username: process.env.DATABASE_USER,
-  databaseDialect: process.env.DATABASE_DIALECT || 'postgres',
-  env: process.env.NODE_ENV || 'development',
-  password: process.env.password || null,
-  host: process.env.HOST,
+  env: envVars.NODE_ENV || 'development',
+  port: envVars.PORT,
+  databaseName: envVars.DATABASE,
+  dbUsername: envVars.DATABASE_USER,
+  databaseDialect: envVars.DATABASE_DIALECT,
+  dbPassword: envVars.password,
+  host: envVars.HOST,
 };
 
 module.exports = config;
