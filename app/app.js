b51dca3473558035239b4cd3912e8502727de2ee
Rodgers Kishagha <mkishagha@gmail.com>
2019-03-05 12:44:36 +0300

    feat(app): initialize passport

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const passport = require('passport');
 const routes = require('./routes')
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+app.use(passport.initialize());
 
 routes(app);
 
