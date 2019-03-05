f4eff35607276835eeca1878ddc73645d3894407
Rodgers Kishagha <mkishagha@gmail.com>
2019-03-05 12:43:26 +0300

    feat(config): configure passport local and jwt auth

---
+++ config/passportSetup.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const { secretKey } = require('./config');
+const db = require('../database/models');
+const EncryptData = require('../lib/helpers/Encrypt');
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+}, async (email, password, done) => {
+  try {
+    const user = await db.User.findOne({ where: { email } });
+    if (!user) return done('user does not exist')
+    const passwordMatch = await EncryptData.comparePassword(password, user.password);
+    if (passwordMatch) return done(null, user)
+    return done('incorrect email or password');
+  } catch (error) {
+    done(error);
+  }
+}));
+
+passport.use(new JwtStrategy({
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secretKey,
+},
+(jwtPayload, done) => {
+  if (Date.now() > jwtPayload.expires) {
+    return done('jwt expired');
+  }
+  return done(null, jwtPayload);
+}));
+
+const localAuthentication = passport.authenticate('local', {
+  session: false,
+})
+
+const jwtAuthentication = passport.authenticate('jwt')
+
+module.exports = {
+  localAuthentication,
+  jwtAuthentication,
+}
