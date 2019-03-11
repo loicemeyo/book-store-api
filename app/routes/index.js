a27a1bc550a02113e8bbcec28656c409100ae678
Rodgers Kishagha <mkishagha@gmail.com>
2019-03-11 09:25:15 +0300

    move the wild card route to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,4 @@
+const HttpStatus = require('http-status');
 const BookRouter = require('./books');
 const UsersRouter = require('./users');
 const AuthRouter = require('./auth');
@@ -6,8 +7,9 @@ const apiPrefix = '/api/v1';
 
 const routes = (app) => {
   app.use(apiPrefix, BookRouter);
-  app.use(apiPrefix, UsersRouter);
   app.use(apiPrefix, AuthRouter);
+  app.use(apiPrefix, UsersRouter);
+  app.use('*', (req, res) => res.status(HttpStatus.NOT_FOUND).json({ message: 'we do not do that here' }))
   return app;
 };
 
